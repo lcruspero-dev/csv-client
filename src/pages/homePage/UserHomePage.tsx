6376a9855a4783ffadb7126e327e3e154772a3a2
Arvin Bautista <denzarvinz@gmail.com>
2025-05-02 15:47:14 +0800

    enhance ui

--- src/pages/homePage/UserHomePage.tsx
+++ src/pages/homePage/UserHomePage.tsx
@@ -87,11 +87,11 @@ const UserHome = () => {
   return (
     <>
       <SurveyModal />
-      <section className="heading container text-center pt-5">
-        <h1 className="text-4xl p-2 drop-shadow-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#1638df] to-[#192fb4]">
-          What do you need help with?
+      <section className="heading container text-center pt-4">
+        <h1 className="italic text-3xl p-1 drop-shadow-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#1638df] to-[#192fb4]">
+          Everything you need is just a click away!
         </h1>
-        <p className="text-2xl font-bold">Please choose from an option below</p>
+        <p className="text-2xl font-bold italic">Select an option to proceed</p>
       </section>
       <div className="container grid grid-cols-3 gap-5 mt-5 text-center p-5 drop-shadow-lg w-6/12">
         <Card
@@ -99,7 +99,9 @@ const UserHome = () => {
           onClick={() => navigate("/timetracker")}
         >
           <img src={timetracker} alt="Time tracker" />
-          <p className="py-3 font-bold">Time Tracker</p>
+          <h3 className="text-lg font-semibold text-gray-800 ">Time Tracker</h3>
+
+          <p className="text-sm text-gray-500 mb-3">Track your work hours</p>
         </Card>
         <Card
           className="relative hover:scale-105 ease-in-out duration-200 cursor-pointer hover:border-1 hover:border-[#5a95ff]"
@@ -111,28 +113,36 @@ const UserHome = () => {
             </div>
           )}
           <img src={memo} alt="memo" />
-          <p className="py-3 font-bold">Memo</p>
+          <h3 className="text-lg font-semibold text-gray-800">Memo</h3>
+          <p className="text-sm text-gray-500 mb-3">
+            View company announcements
+          </p>
         </Card>
         <Card
           className="hover:scale-105 ease-in-out duration-200 cursor-pointer hover:border-1 hover:border-[#5a95ff]"
           onClick={() => navigate("/request-something")}
         >
           <img src={request} alt="test" />
-          <p className="py-3 font-bold">HR Support Request</p>
+          <h3 className="text-lg font-semibold text-gray-800">HR Support</h3>
+          <p className="text-sm text-gray-500 mb-3">Request HR assistance</p>
         </Card>
         <Card
           className="hover:scale-105 ease-in-out duration-200 cursor-pointer hover:border-1 hover:border-[#5a95ff]"
           onClick={() => navigate("/create-ticket")}
         >
           <img src={gethelp} alt="test" />
-          <p className="py-3 font-bold">IT Support Request</p>
+          <h3 className="text-lg font-semibold text-gray-800">IT Support</h3>
+          <p className="text-sm text-gray-500 mb-3">Get technical help</p>
         </Card>
         <Card
           className="hover:scale-105 ease-in-out duration-200 cursor-pointer hover:border-1 hover:border-[#5a95ff]"
           onClick={() => navigate("/view-ticket")}
         >
           <img src={ticket} alt="tickets" />
-          <p className="py-3 font-bold">View My Tickets</p>
+          <h3 className="text-lg font-semibold text-gray-800">My Tickets</h3>
+          <p className="text-sm text-gray-500 mb-3">
+            View your support tickets
+          </p>
         </Card>
         <TooltipProvider>
           {nteTooltip ? (
@@ -165,7 +175,12 @@ const UserHome = () => {
               <div className="relative">
                 <img src={test} alt="NTE" className="w-full" />
               </div>
-              <p className="py-3 font-bold text-center">Employee Notice</p>
+              <h3 className="text-lg font-semibold text-gray-800">
+                Employee Notice
+              </h3>
+              <p className="text-sm text-gray-500 mb-3">
+                View disciplinary notice
+              </p>
             </Card>
           )}
         </TooltipProvider>
